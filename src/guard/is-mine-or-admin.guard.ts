64e81fd35cc8a6b5dfee414d83a4a169df16c12c
tatahyeonv <xhrl523@naver.com>
2024-08-06 20:02:43 +0900

    chore: IsMineOrAdminGuard 수정

--- src/guard/is-mine-or-admin.guard.ts
+++ src/guard/is-mine-or-admin.guard.ts
@@ -10,25 +10,25 @@ export class IsAdminGuard implements CanActivate {
     if (!user) {
       throw new UnauthorizedException('사용자 정보를 가져올 수 없습니다.');
     }
-    if (user.role === RolesEnum.ADMIN) {
-      return true;
+    if (user.role !== RolesEnum.ADMIN) {
+      throw new ForbiddenException('관리자 권한이 없습니다.');
     }
-
     return true;
   }
 }
 
-export class IsMineOrAdminGuard extends IsAdminGuard {
+export class IsMineOrAdminGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    await super.canActivate(context);
-
     const req = context.switchToHttp().getRequest();
+    if (req.user.role === RolesEnum.ADMIN) {
+      return true;
+    }
     const userId = req.params.id;
     if (!userId) {
       throw new BadRequestException('User ID가 파라미터로 제공되어야 합니다.');
     }
     if (req.user.id !== +userId) {
-      throw new ForbiddenException('자신의 계정만 조회하거나 수정할 수 있습니다.');
+      throw new ForbiddenException('본인 계정만 조회하거나 수정할 수 있습니다.');
     }
 
     return true;
